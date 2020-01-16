f1aaaa86628f58e5a8357cc4bd5952b172594b4d
eros francesco <eros.francesco.francesco@gmail.com>
2020-01-16 12:25:22 +0100

    Header bar and sass mixins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameWrapperComponent } from './game-wrapper/game-wrapper.component';
+import { HeaderBarComponent } from './game-wrapper/header-bar/header-bar.component';
+import { GameScoreComponent } from './game-score/game-score.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameWrapperComponent
+    GameWrapperComponent,
+    HeaderBarComponent,
+    GameScoreComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/game-wrapper/header-bar/header-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-bar',
+  templateUrl: './header-bar.component.html',
+  styleUrls: ['./header-bar.component.scss']
+})
+export class HeaderBarComponent implements OnInit {
+
+  availableTypes: string[] = ['ROCK', 'PAPER', 'SCISSORS'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
